4d3644c39b554e87cc0b4d4a58ec64f21c28c4dd
Ben Schmidt <bmschmidt@gmail.com>
2013-01-17 14:55:29 -0500

    newmapchart files are prettier and match Bostocks specification for reusability

--- beta/legends.js
+++ beta/legends.js
@@ -1,4 +1,86 @@
-makeFillLegend = function () {
+fillLegendMaker = function(colorscale) {
+    
+    var yrange = [75,500]
+    colorticks = colorscale.ticks(15);
+
+    width = 25
+    xpos  = 20
+    plot = true
+    title = "Per Million Words"
+    colorpoints = colorLegend.selectAll('rect')
+    colorlabels = colorLegend.selectAll('text')
+
+    function my() {
+	colorLegend.selectAll().remove()
+
+	var data1 = d3.range(50);
+
+	//because I'm doing some non-Kosher multi-element color scales, I'm getting a little ugly here.
+
+	scaleRects = colorLegend.selectAll("rect")
+	    .data(data1);
+
+	colorScaleLength = colorscale.domain().length
+	
+	singularize = d3.scale.linear()
+	    .range([d3.min(data1), d3.max(data1)])
+	    .domain([0,colorScaleLength])
+	
+	var colorScale = d3.scale.linear()
+	    .domain(d3.range(colorScaleLength).map(function(n) {return singularize(n)} ))
+	    .range(colorscale.range());
+
+	var colorAxisScale = d3.scale.linear()
+	    .range(yrange)
+	    .domain(d3.extent(data1))
+
+	scaleRects.enter()
+	    .append("rect")
+	    .attr({
+		width: width,
+		height: (yrange[1]-yrange[0])/data1.length + 1,
+		y: function(d) { return colorAxisScale(d)},
+		x: xpos,
+		fill: function(d) {
+		    return colorScale(d);
+		}
+	    })
+	
+        colorlabels.remove()
+
+	nticks = 10
+
+	//the smoother sometimes mixes things up on you.
+	nticks = colorscale.ticks(nticks).length
+
+	var colorAxisScale = d3.scale.linear()
+	    .domain([0,nticks-1])
+	    .range([yrange[0]+5,yrange[1]-5])
+
+	//I'm clearly not getting something here.
+	colorlabels = colorLegend.selectAll('text').data(d3.range(nticks))
+
+        colorlabels.enter().append('text')
+	    .attr('y',function(d) {return colorAxisScale(d)})
+            .attr('x', xpos + width +2)
+	    .attr('dy',	  '0.5ex')
+	    .attr("text-anchor", "left")
+            .attr('fill','white')
+            .text(function(d) {return(prettyName(colorscale.ticks(nticks)[d]))})
+
+	
+        text1 = "Usage of '" + query['search_limits']['word'][0] + "'" +   " per Million Words"
+        if (comparisontype()=='comparison') {
+            text1 = "Usage of '" + query['search_limits']['word'][0] + "'" + " per use of '" + query['compare_limits']['word'][0] + "'"
+        }
+        colorLegend.append('text').attr('x',xpos+375).attr('y',yrange[0]-25).text(text1).attr('fill','white').attr('font-size',35).attr('font-family',"Arial")
+    }
+
+    return my
+}
+
+
+fmakeFillLegend = function () {
     colorLegend.yrange = [75,500]
     colorticks = colorscale.ticks();
 
@@ -15,7 +97,7 @@ makeFillLegend = function () {
     colorLegend.update = function() {
         colorLegend.data = d3.range(colorticks.length).map(function(n) {
             value =
-                {"label":prettyName(colorticks[n]*1000000),
+                {"label":prettyName(colorticks[n]),
                  "color":colorscale(colorticks[n]),
                  'y':colorLegend.scale(n)
                 }
@@ -59,7 +141,7 @@ makeSizeLegend = function() {
     thisLegend = sizeLegend
 
     thisLegend.sourceScale = sizescale
-    myticks = thisLegend.sourceScale.ticks(5);
+    myticks = thisLegend.sourceScale.ticks(10);
     thisLegend.yrange = [300,500]
     thisLegend.boxheight = (thisLegend.yrange[1]-thisLegend.yrange[0])/myticks.length +1.5
     thisLegend.width = 25
@@ -73,11 +155,8 @@ makeSizeLegend = function() {
         .range(thisLegend.yrange)
         .domain(d3.extent(d3.range(myticks.length)))
 
-    console.log(myticks);
-
     thisLegend.update = function() {
         thisLegend.data = d3.range(myticks.length).map(function(n) {
-            console.log(myticks[n])
             value =
                 {"label":prettyName(myticks[n]),
                  "scaled":thisLegend.sourceScale(myticks[n]),
