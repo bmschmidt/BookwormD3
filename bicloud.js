78db8b5dbc8706f140d5e18e166571856a0d33dc
Benjamin Schmidt <bmschmidt@gmail.com>
2013-11-30 15:14:00 -0500

    new module system

--- bicloud.js
+++ bicloud.js
@@ -1,3 +1,16 @@
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor(i * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
+subarray = getRandomSubarray(allwords,44)
+
 //There should always be a query variable present: from that, it should be possible to derive anything else we'll ever need, and any changes can update it directly.
 //This is the cardinal rule of the architecture here: absolutely any state must DRAW FROM and UPDATE the query variable if it pertains to any higher-level architecture
 //about what's being displayed.
@@ -6,52 +19,44 @@
 
 //Really, the query should be factory so we could have multiple ones present at a time--but that would create all sorts of weird visualization cases we don't need actually to worry about.
 
-var query = {};
+var bookworm = new Bookworm()
 
-var defaultQuery = {
-    "method":"return_json",
-    "words_collation":"Case_Sensitive",
-    "database":"ChronAm",
-    "search_limits":{
-        "date_year":{"$lte":1922,"$gte":1850},
-        "word":["Apache"]
-    },
-    "aesthetic":{'x':'lng','y':'lat',"size":"WordCount","color":"WordsPerMillion"},
-    "plotType":"map"
-};
-
-if (window.location.host=="melville.seas.harvard.edu" | window.location.host=="benschmidt.org") {
-    defaultQuery = {
+    bookworm.query = {
         "method":"return_json",
         "words_collation":"Case_Sensitive",
-        "groups":["year","classification"],
-        "database":"presidio",
-        "counttype":["WordCount","TotalWords","WordsPerMillion"],
+        "database":"OL",
         "search_limits":{
-            "year":{"$lte":1922,"$gte":1850},
-            "word":["chart"]
+            "lc1":["PR"],
+            "word": subarray
         },
-        "aesthetic":{"x":"yearchunk","y":"classification","color":"WordsPerMillion"},
-        "plotType":"heatMap"
+        "compare_limits":{
+            "lc1":["PS"],
+            "word":subarray
+        },
+        "counttype":["WordCount","TotalWords"],
+        "groups":["unigram"],
+        "aesthetic":{"label":"unigram","x":"WordCount","y":"TotalWords"},
+        "plotType":"bicloud"
     }
+
+
+bookworm.changePlotType = function() {
+    console.log(this.query.plotType)
 }
 
-if (window.location.host=="localhost:8080") {
-    console.log("yo, localhost!")
-    defaultQuery = {
-        "method":"return_json",
-        "words_collation":"Case_Sensitive",
-        "groups":["publication_date_year","publication_date_month_year"],
-        "database":"halftimes",
-        "counttype":["WordCount","TotalWords","WordsPerMillion"],
-        "search_limits":{
-            "word":["war"]
-        },
-        "plotType":"heatMap"
-    }
+var points;
+
+
+
+changeWords = function() {
+    words = getRandomSubarray(allwords,44)
+    bookworm.query.search_limits['word'] = bookworm.query.compare_limits['word'] = words
+    bookworm.queryAligner.updateQuery()
+    bookworm.updateData(bookworm.bicloud,append=true)
 }
-var plotTransformers = {};
-var lastPlotted="None"
+d3.selectAll("text")
+
+
 
 //Graphical Elements
 var w = window.innerWidth
@@ -62,8 +67,6 @@ var svg = d3.select("#svg")
     .attr('width',window.innerWidth)
     .attr('height',window.innerHeight*.9)
 
-var width;
-
 //These are the things to delete when a new chart is refreshed.
 //They contain the various aspects of the actual plot
 
@@ -73,35 +76,8 @@ var paperdiv = svg
     .append("g")
     .attr("id","#paperdiv")
 
-//These datums (that's the plural of datum, you know) are there to 
-//make the item draggable using the "drag" function.
-
-paperdiv.datum({"x":0,"y":0})
-
-//var legend = svg.append('g').attr('id','legend');
-//var colorLegend = legend.append('g').attr('id','colorLegend').attr('transform','translate(' + w/25+ ','+h/7+')');
 
 var title = svg.append('g').attr('id','title').attr('transform','translate(' + w*.4+ ',' + 25  +')');
-
-
-// Things for the background map
-
-var projection = {"mapname":"none"}
-var stateItems;
-
-// Variables for the grid charts
-var testing, x,y;
-
-// Prepare the paper points.
-// These variables should be renamed for clarity: 'paper' refers to newspapers, for legacy reasons.
-var paperdata = [];
-
-var colorLegendPointer,
-updatePointer,
-sizeAxis,
-titleText,
-legendScale = d3.scale.linear()
-;
 
 var nameSubstitutions = {
     "WordsPerMillion":"Uses per Million Words",
@@ -134,8 +110,9 @@ if(window.location.hash) {
     var hash = window.location.hash.substring(1);
     decoded = decodeURIComponent(hash)
     query =  JSON.parse(decoded)
+    bookworm.query=query
 } else {
-    query = defaultQuery
+    query = bookworm.query
 }
 
 if (query['scaleType']==undefined) {
@@ -151,7 +128,7 @@ if (!('aesthetic' in query)) {
     }
 }
 
-updateAxisOptionBoxes()
+//updateAxisOptionBoxes()
 
 
 // And for now I'm just having that query live in a text box. We can use the real Bookworm query entries instead, but no use reinventing that wheel here.
@@ -167,14 +144,14 @@ var APIbox = d3.select('#APIbox')
 d3.selectAll("[bindTo]")
     .on('change',function() {
         console.log("change registered")
-        queryAligner.updateQuery(d3.select(this))
+        bookworm.queryAligner.updateQuery(d3.select(this))
     })
     .on('keyup',function() {
         console.log("keyup registered")
-        queryAligner.updateQuery(d3.select(this))
+        bookworm.queryAligner.updateQuery(d3.select(this))
     })
 
-queryAligner.updateQuery()
+bookworm.queryAligner.updateQuery()
 
 d3.select("body").append("button")
     .text('Redraw Plot')
@@ -183,63 +160,6 @@ d3.select("body").append("button")
         currentPlot()
     })
 
-
-d3.select("#AdvancedOptions")
-//Make advanced Options into a toggler.
-    .on("click",function() {
-        display = d3.select(".debugging")
-        display = (display.style("display"))
-
-        if (display=="none") {
-
-            d3.selectAll(".debugging")
-                .style("display","inline")
-            d3
-                .select(this)
-                .text("Hide Advanced")
-        }
-        if (display=="inline") {
-            d3.selectAll(".debugging").style("display","none")
-            d3.select(this).text("Show Advanced")
-        }
-    })
-
-d3.select("#ExportData")
-//Make advanced Options into a toggler.
-    .on("click",function() {
-        function post_to_url(path, params, method) {
-            //Function From http://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
-            method = method || "post"; // Set method to post by default, if not specified.
-
-            // The rest of this code assumes you are not using a library.
-            // It can be made less wordy if you use one.
-            var form = document.createElement("form");
-            form.setAttribute("method", method);
-            form.setAttribute("action", path);
-
-            for(var key in params) {
-                if(params.hasOwnProperty(key)) {
-                    var hiddenField = document.createElement("input");
-                    hiddenField.setAttribute("type", "hidden");
-                    hiddenField.setAttribute("name", key);
-                    hiddenField.setAttribute("value", params[key]);
-
-                    form.appendChild(hiddenField);
-                }
-            }
-            console.log(params)
-            document.body.appendChild(form);
-            form.submit();
-        }
-
-        localquery = JSON.parse(JSON.stringify(query))
-        localquery['method'] = "return_tsv"
-        post_to_url("/cgi-bin/dbbindings.py",   {"queryTerms":JSON.stringify(localquery)})
-    })
-
-
-
-var lastOptions = $('<div />');
 
 //The types of maps are just coded in.
 mapOptions = [
@@ -279,18 +199,17 @@ PuOr = ['rgb(84,39,136)','rgb(153,142,195)','rgb(216,218,235)','rgb(247,247,247)
 RdYlGn = ["#D61818","#FFAE63","#FFFFBD","#B5E384"].reverse()
 RdYlGn = colorbrewer["RdYlGn"][5].slice(0,4).reverse()
 
-//RdYlGn = greenToRed
-
 //define some default scales
 nwords = d3.scale.sqrt().range([0,100]);
 var sizescale = nwords
 var colorscale = d3.scale.log().range(greenToRed);
 
 //Have to keep the data the same for subsequent calls, but this will transform them
+plotTransformers = {};
 dataTypes = {};
 
 var legendData = [];
 
-var currentPlot=myPlot()
-currentPlot()
-d3.selectAll(".debugging").style('display','none')
+//var currentPlot=myPlot()
+//currentPlot()
+//d3.selectAll(".debugging").style('display','none')
