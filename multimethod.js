a848f9c9d972fd863eb54a9f43d53b65c8b0f681
Benjamin Schmidt <bmschmidt@gmail.com>
2013-12-02 22:26:59 -0500

    using window hashes

--- multimethod.js
+++ multimethod.js
@@ -7,7 +7,7 @@
 //Really, the query should be factory so we could have multiple ones present at a time--but that would create all sorts of weird visualization cases we don't need actually to worry about.
 
 
-query = {
+dquery = {
     "method":"return_json",
     "words_collation":"Case_Sensitive",
     "database":"ol",
@@ -17,6 +17,18 @@ query = {
     },
     "aesthetic":{"x":"lc0","y":"lc1","color":"WordCount"},
     "plotType":"heatmap"
+}
+
+dquery = {
+    "method":"return_json",
+    "words_collation":"Case_Sensitive",
+    "database":"ol",
+    "search_limits":{
+        "lc0":{"$ne":""},
+        "word":["gegen"]
+    },
+    "aesthetic":{"x":"WordCount","level1":"publish_country","level2":"publish_places","level3":"lc0"},
+    "plotType":"sunburst"
 }
 
 
@@ -30,92 +42,21 @@ var svg = d3.select("#svg")
 
 dataTypes = {};
 
-d3.select("body").on("keypress",function(e){
-    if(d3.event.keyCode == 13){
-        bookworm.updatePlot()
-    }
-});
-
-
-
-d3.selectAll("[bindTo]")
-    .on('change',function() {
-        console.log("change registered")
-        bookworm.updateQuery(d3.select(this))
-    })
-    .on('keyup',function() {
-        console.log("keyup registered")
-        bookworm.updateQuery(d3.select(this))
-    })
 
 
 /**
-//bookworm.queryAligner.alignAesthetic();
-//bookworm.updateData()
-
-
-//A sunburst function that starts with unnested data.
-
-d3.select(self.frameElement).style("height", height + "px");
-
-var points;
-
-d3.selectAll("text")
-
-
-
-//Graphical Elements
-var w = window.innerWidth
 var h = window.innerHeight
 
 var bottomLevel = svg.append("g").attr("id", "#bottomLevel")
 
 var maplevel = svg.append("g").attr("id", "#maplevel")
-var paperdiv = svg
-    .append("g")
-    .attr("id","#paperdiv")
-
 
 var title = svg.append('g').attr('id','title').attr('transform','translate(' + w*.4+ ',' + 25  +')');
 
 
-
-
-
-
 //Pull query from hash location iff supplied
-if(window.location.hash) {
-    var hash = window.location.hash.substring(1);
-    decoded = decodeURIComponent(hash)
-    query =  JSON.parse(decoded)
-    bookworm.query=query
-} else {
-    query = bookworm.query
-}
-
-if (query['scaleType']==undefined) {
-    query['scaleType'] = "linear"
-}
-
-
-if (!('aesthetic' in query)) {
-    query['aesthetic']= {
-        "color":"WordsPerMillion",
-        "size":"WordCount",
-        "filterByTop":"WordCount"
-    }
-}
-
-//updateAxisOptionBoxes()
-
 
 // And for now I'm just having that query live in a text box. We can use the real Bookworm query entries instead, but no use reinventing that wheel here.
-
-var APIbox = d3.select('#APIbox')
-    .property('value', JSON.stringify(query))
-    .attr('style', 'width: 95%;')
-    .on('keyup',function(){})
-
 
 //Well, one re-invention: a word box at the top that automatically updates the text box, and vice-versa.
 
@@ -130,20 +71,9 @@ var APIbox = d3.select('#APIbox')
 //  .text(function(d) {return(d.text)})
 
 var options = $('<div />');
-
 //executeButtons.appendTo($('body'));
 
 
-
-//I like this pallette--I think we usually need two tones to really discriminate,
-//even though dataviz wisdom seems to say that's not kosher.
-
-greenToRed = ["#D61818","#FFAE63","#FFFFBD","#B5E384"].reverse()
-RdYlGn = greenToRed
-PuOr = ['rgb(84,39,136)','rgb(153,142,195)','rgb(216,218,235)','rgb(247,247,247)','rgb(254,224,182)','rgb(230,97,1)']
-
-
-//define some default scales
 nwords = d3.scale.sqrt().range([0,100]);
 var sizescale = nwords
 var colorscale = d3.scale.log().range(greenToRed);
@@ -160,6 +90,17 @@ var legendData = [];
 **/
 
 
-var bookworm = Bookworm(query)
+//pull location hash:
+
+if(window.location.hash) {
+    var hash = window.location.hash.substring(1);
+    decoded = decodeURIComponent(hash)
+    dquery =  JSON.parse(decoded)
+    dquery=dquery
+} else {
+    dquery = dquery
+}
+var bookworm = Bookworm(dquery)
+
 
 bookworm.updatePlot()
