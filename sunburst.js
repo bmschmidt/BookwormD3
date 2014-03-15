402c8cf7ce5989614b9c8f690c71ec82587fb463
Benjamin Schmidt <bmschmidt@gmail.com>
2014-03-15 19:03:15 -0400

    sunburst layout

--- sunburst.js
+++ sunburst.js
@@ -33,7 +33,7 @@ dataTypes = {};
 
 d3.select("body").on("keypress",function(e){
     if(d3.event.keyCode == 13){
-        bookworm.newPlot()
+        bookworm.updatePlot()
     }
 });
 
@@ -49,133 +49,8 @@ d3.selectAll("[bindTo]")
         bookworm.updateQuery(d3.select(this))
     })
 
-
-/**
-//bookworm.queryAligner.alignAesthetic();
-//bookworm.updateData()
-
-
-//A sunburst function that starts with unnested data.
-
-d3.select(self.frameElement).style("height", height + "px");
-
-var points;
-
-d3.selectAll("text")
-
-
-
-//Graphical Elements
-var w = window.innerWidth
-var h = window.innerHeight
-
-var bottomLevel = svg.append("g").attr("id", "#bottomLevel")
-
-var maplevel = svg.append("g").attr("id", "#maplevel")
-var paperdiv = svg
-    .append("g")
-    .attr("id","#paperdiv")
-
-
-var title = svg.append('g').attr('id','title').attr('transform','translate(' + w*.4+ ',' + 25  +')');
-
-
-var nameSubstitutions = {
-    "WordsPerMillion":"Uses per Million Words",
-    "WordCount":"# of matches",
-    "TextPercent":"% of texts",
-    "TotalWords":"Total # of words",
-    "TextCount":"# of Texts",
-    "TotalTexts":"Total # of Texts"
-}
-
-for (item in bookworm.quantitativeVariables) {
-    nameSubstitutions[item.variable] = item.label
-}
-
-
-
-//Pull query from hash location iff supplied
-if(window.location.hash) {
-    var hash = window.location.hash.substring(1);
-    decoded = decodeURIComponent(hash)
-    query =  JSON.parse(decoded)
-    bookworm.query=query
-} else {
-    query = bookworm.query
-}
-
-if (query['scaleType']==undefined) {
-    query['scaleType'] = "linear"
-}
-
-
-if (!('aesthetic' in query)) {
-    query['aesthetic']= {
-        "color":"WordsPerMillion",
-        "size":"WordCount",
-        "filterByTop":"WordCount"
-    }
-}
-
-//updateAxisOptionBoxes()
-
-
-// And for now I'm just having that query live in a text box. We can use the real Bookworm query entries instead, but no use reinventing that wheel here.
-
-var APIbox = d3.select('#APIbox')
-    .property('value', JSON.stringify(query))
-    .attr('style', 'width: 95%;')
-    .on('keyup',function(){})
-
-
-//Well, one re-invention: a word box at the top that automatically updates the text box, and vice-versa.
-
-
-
-//    mapSelector = d3.select("#lastOptions").append('select').attr('id',"mapChoice").attr('class',"chartSpecific mapChart")
-//    pointSelector = mapSelector.selectAll('option').data(mapOptions)
-
-//    pointSelector.enter()
-//      .append('option')
-//    .attr('value',function(d){return d.value})
-//  .text(function(d) {return(d.text)})
-
-var options = $('<div />');
-
-//executeButtons.appendTo($('body'));
-
-
-
-//I like this pallette--I think we usually need two tones to really discriminate,
-//even though dataviz wisdom seems to say that's not kosher.
-
-greenToRed = ["#D61818","#FFAE63","#FFFFBD","#B5E384"].reverse()
-RdYlGn = ['rgb(26,152,80)','rgb(255,255,191)','rgb(215,48,39)']
-RdYlGn = greenToRed
-PuOr = ['rgb(84,39,136)','rgb(153,142,195)','rgb(216,218,235)','rgb(247,247,247)','rgb(254,224,182)','rgb(230,97,1)']
-RdYlGn = ["#D61818","#FFAE63","#FFFFBD","#B5E384"].reverse()
-RdYlGn = colorbrewer["RdYlGn"][5].slice(0,4).reverse()
-
-//define some default scales
-nwords = d3.scale.sqrt().range([0,100]);
-var sizescale = nwords
-var colorscale = d3.scale.log().range(greenToRed);
-
-//Have to keep the data the same for subsequent calls, but this will transform them
-plotTransformers = {};
-
-
-var legendData = [];
-
-//var currentPlot=myPlot()
-//currentPlot()
-//d3.selectAll(".debugging").style('display','none')
-**/
-
-
 var bookworm = Bookworm(query)
 
 bookworm.updateQuery()
 bookworm.alignAesthetic()
-bookworm.newPlot()
+bookworm.updatePlot()
