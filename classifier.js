cd9adf5e3cb73c76a9f21f96ee2285e1b7278af1
Benjamin Schmidt <bmschmidt@gmail.com>
2014-03-19 12:39:29 -0400

    cool old script to classify by word freqs

--- classifier.js
+++ classifier.js
@@ -3,18 +3,21 @@ var bookworm = new Bookworm({
     "words_collation":"Case_Sensitive",
     "database":"presidio",
     "search_limits":{
-        "word": ["Call","me","Ishmael","","","Some","years","ago","","never","mind","how","long","","precisely","","having","little","or"]
+        "word": ["Call","me","Ishmael","","","Some","years","ago","","never","mind","how","long","","precisely","","having","little","or"],
+	"year":{"$gte":1830,"$lte":1922},
     },
     "counttype":["WordCount"],
-    "groups":["unigram","classification"],
     "aesthetic":{"label":"unigram","y":"WordCount","x":"classification"},
     "plotType":"logClassify"
 })
 
-var changeWords;
+var changeWords,inspect;
 bookworm.updateAxisOptionBoxes();
+
+
 d3.select("svg").on("click",function(d) {
-    var value =         document
+    d3.selectAll("textarea").style("display","none")
+    var value =  document
         .getElementById("paragraph")
         .value
 
@@ -27,28 +30,76 @@ d3.select("svg").on("click",function(d) {
     allwords = allwords.filter(function(d) {return(d!="")})
 
     allwords = allwords.filter(function(d) {return(d!="")})
-    var nn=1;
-    var ii=0;
-    var myVar;
-    var ticker = 1;
-    words = allwords.slice(ii,ii+nn)
-    bookworm.query.search_limits['word'] = words
+
+    var textDisplay = d3.selectAll("#textDisplay")
+
+    if (textDisplay[0].length==0) {
+	textDisplay =d3.select("svg")
+	    .append("svg")
+	    .attr("height",100)
+	    .attr("width",d3.select("svg").style("width"))
+	    .attr("id","textDisplay")
+	    .append("g")
+	    .attr("transform","translate(0,35)")
+	    .attr("id","paraGroup")
+    }
+
+    style = "font-size:18pt;fill:grey;"
+
+    var displayWords = d3.layout.paragraph()
+	.label(function(d) {return [d]})
+	.style(style)
+	.width(d3.select("#textDisplay").attr("width").replace("px",""))
+	.points(allwords);
+
+    var displayedWords = textDisplay.selectAll("text").data(displayWords);
+
+    inspect = displayedWords
+
+    displayedWords.enter().append("text")
+	.attr("x",function(d) {return d.x})
+	.attr("y",function(d) {return d.y})
+	.text(function(d) {return d.label})
+	.classed("unselected",true)
+
+    displayedWords.attr("style",style)
+
     changeWords = function() {
-        words = allwords.slice(ii,ii+nn)
+
+	workingOn = textDisplay.select("text.unselected")
+
+	words = workingOn.text();//datum().label;
+
         bookworm.query.search_limits['word'] = words
+
+	workingOn
+	    .classed("unselected",false)
+	    .transition()
+	    .duration(300)
+	    .style('fill','red')
+	    .transition()
+	    .attr("y",120).attr("x",400)
+	    .duration(2000)
+	
+	    .style("font-size",180)
+	    .style("opacity",0)
+	    .remove()
+
+	min = d3.min(d3.selectAll("text.unselected").data().map(function(d) {return d.y}));
+	
+	textDisplay.selectAll("text.unselected")
+	    .transition().duration(800)
+	    .attr("y",function(d) {return (d.y-min)})
+	
+
         if (words.length>0){
             bookworm.updateData("logClassify")
         }
-        ii = ii+nn;
-        ticker++;
     }
-
-    console.log(bookworm.query.search_limits.word);
-
-    changeWords()
 
     d3.select("svg").on("click",function(d) {
         changeWords()
+	myVar = setInterval(changeWords,300);
     })
 
 })
@@ -80,16 +131,9 @@ d3.select("body").append("button")
     })
 
 
-//The types of maps are just coded in.
-
 d3.select("body").on("keypress",function(e){
     if(d3.event.keyCode == 13){
         bookworm.updatePlot()
     }
 });
 
-
-//I like this pallette--I think we usually need two tones to really discriminate,
-//even though dataviz wisdom seems to say that's not kosher.
-
-//define some default scales
